b213887a2fc0170af852dffe6cfb324173eb46c9
dochoamerc <dochoamerc@miners.utep.edu>
2022-06-13 18:27:25 -0600

    removed test

--- draw.h
+++ draw.h
@@ -7,7 +7,6 @@ void print_arrow(int startCol, int size);
 void print_char_11x16(char c);
 void print_char_5x7(char c);
 void print_char_8x12(char c);
-void print_char_7x5(char c);
 
 extern const unsigned short font_11x16[95][11];
 extern const unsigned char font_5x7[96][5];
